7901ef0fd272b5996a3517f215cd43536b60f36e
mschemmel <mschemmel@online.de>
2020-07-17 23:14:08 +0200

    test for empty edge list

--- corrnet.py
+++ corrnet.py
@@ -17,43 +17,45 @@ def main():
 
 
   if (args.corrmat and args.pmat):
-      # check correlation limit and pvalue limit
+      # check correlation threshold and pvalue threshold
       clim = args.corrlimit if args.corrlimit else 0.7
       plim = args.plimit if args.plimit else 0.05
       
       # create new edgelist object and pull edges
       data = cte.edgelist(args.corrmat, args.pmat, clim, plim) 
-      edges = data.edges()
 
       # check if output path is provided
       output = args.output if args.output else os.getcwd()
       pref = args.prefix if args.prefix else "corr_default"
       
-      # create network
-      data_net = etn.net(edges)
-      print("")
-      print("## Network Summary")
-      for element in data_net.summary():
-          print(element)
-      
-      # save all files
-      # create final output folder
-      out_dir = os.path.join(output, pref)
-      try:
-          if os.path.isdir(out_dir):
-              pass
-          else:
-              os.mkdir(out_dir)
-      except FileNotFoundError:
-          print("ERROR: Output directory could not be created: Path not found.")
-          sys.exit(0)
+      # create network if edges dataframe is not empty
+      edges = data.edges()
+      if not edges.empty:
+          data_net = etn.net(edges)
+          print("")
+          print("## Network Summary")
+          for element in data_net.summary():
+              print(element)
+          
+          # save all files 
+          # create final output folder
+          out_dir = os.path.join(output, pref)
+          try:
+              if os.path.isdir(out_dir):
+                  pass
+              else:
+                  os.mkdir(out_dir)
+          except FileNotFoundError:
+              print("ERROR: Output directory could not be created: Path not found.")
+              sys.exit(0)
 
-      edges.to_csv("{}/edge_list_{}.tsv".format(out_dir, pref), index = None, sep = '\t', mode = 'w')
-      network = data_net.plot_network()
-      network.savefig(os.path.join(out_dir, "network.pdf"), bbox_inches = 'tight')
-      
-      degree_histogram = data_net.plot_degree_hist()
-      degree_histogram.savefig(os.path.join(out_dir, "histogram.pdf"), bbox_inches = 'tight')
-
+          edges.to_csv("{}/edge_list_{}.tsv".format(out_dir, pref), index = None, sep = '\t', mode = 'w')
+          network = data_net.plot_network()
+          network.savefig(os.path.join(out_dir, "network.pdf"), bbox_inches = 'tight')
+          
+          degree_histogram = data_net.plot_degree_hist()
+          degree_histogram.savefig(os.path.join(out_dir, "histogram.pdf"), bbox_inches = 'tight') 
+      else:
+        print("ERROR: edge list dataframe is empty. Nothing more to do.") 
 if __name__ == "__main__":
   main()
