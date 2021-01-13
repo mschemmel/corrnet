93af90c1f5db5adc47fab6dbc57839217bd35c90
mschemmel <mschemmel@online.de>
2021-01-13 18:54:08 +0100

    add degree limit argument

--- corrnet.py
+++ corrnet.py
@@ -5,58 +5,59 @@ import os
 import sys
 
 def main():
-  # handle command line arguments
-  parser = argparse.ArgumentParser(description = "Program to transform correlation matrix to edgelists for further network creation.")
-  parser.add_argument("-cm", "--corrmat", help = "Path to correlation matrix")
-  parser.add_argument("-pm", "--pmat", help = "Path to p value matrix")
-  parser.add_argument("-clim", "--corrlimit", help = "correlation value threshold (Default: -0.7 | 0.7")
-  parser.add_argument("-out", "--output", help = "Path for generated summaries and network statistics")
-  parser.add_argument("-plim", "--plimit", help = "p-value threshold (Default: 0.05)")
-  parser.add_argument("-pre", "--prefix", help = "Prefix to label specific run")
-  args = parser.parse_args()
+	# handle command line arguments
+	parser = argparse.ArgumentParser(description = "Program to transform correlation matrix to edgelists for further network creation.")
+	parser.add_argument("-cm", "--corrmat", help = "Path to correlation matrix")
+	parser.add_argument("-pm", "--pmat", help = "Path to p value matrix")
+	parser.add_argument("-clim", "--corrlimit", help = "correlation value threshold (Default: -0.7 | 0.7")
+	parser.add_argument("-plim", "--plimit", help = "p-value threshold (Default: 0.05)")
+	parser.add_argument("-dlim", "--dlimit", help = "degree count threshold (Default: 5")
+	parser.add_argument("-out", "--output", help = "Path for generated summaries and network statistics")
+	parser.add_argument("-pre", "--prefix", help = "Prefix to label specific run")
+	args = parser.parse_args()
 
 
-  if (args.corrmat and args.pmat):
-      # check correlation threshold and pvalue threshold
-      clim = args.corrlimit if args.corrlimit else 0.7
-      plim = args.plimit if args.plimit else 0.05
-      
-      # create new edgelist object and pull edges
-      data = cte.edgelist(args.corrmat, args.pmat, clim, plim) 
+	if (args.corrmat and args.pmat):
+			# check correlation threshold and pvalue threshold
+			clim = args.corrlimit if args.corrlimit else 0.7
+			plim = args.plimit if args.plimit else 0.05
+			dlim = args.dlimit if args.dlimit else 5
+	
+			# create new edgelist object and pull edges
+			data = cte.edgelist(args.corrmat, args.pmat, clim, plim, dlim) 
 
-      # check if output path is provided
-      output = args.output if args.output else os.getcwd()
-      pref = args.prefix if args.prefix else "corr_default"
-      
-      # create network if edges dataframe is not empty
-      edges = data.edges()
-      if not edges.empty:
-          data_net = etn.net(edges)
-          
-          print("")
-          print("## Network Summary")
-          for element in data_net.summary():
-              print(element)
-          
-          # save all files 
-          # create final output folder
-          out_dir = os.path.join(output, pref)
-          try:
-              if os.path.isdir(out_dir):
-                  pass
-              else:
-                  os.mkdir(out_dir)
-          except FileNotFoundError:
-              print("ERROR: Output directory could not be created: Path not found.")
-              sys.exit(0)
+			# check if output path is provided
+			output = args.output if args.output else os.getcwd()
+			pref = args.prefix if args.prefix else "corr_default"
+			
+			# create network if edges dataframe is not empty
+			edges = data.edges()
+			if not edges.empty:
+					data_net = etn.net(edges)
+					print("")
+					print("## Network Summary")
+					for element in data_net.summary():
+							print(element)
+					
+					# save all files 
+					# create final output folder
+					out_dir = os.path.join(output, pref)
+					try:
+							if os.path.isdir(out_dir):
+									pass
+							else:
+									os.mkdir(out_dir)
+					except FileNotFoundError:
+							print("ERROR: Output directory could not be created: Path not found.")
+							sys.exit(0)
 
-          edges.to_csv(f"{out_dir}/edge_list_{pref}.tsv", index = None, sep = '\t', mode = 'w')
-          network = data_net.plot_network()
-          network.savefig(os.path.join(out_dir, "network.pdf"), bbox_inches = 'tight')
-          
-          degree_histogram = data_net.plot_degree_hist()
-          degree_histogram.savefig(os.path.join(out_dir, "histogram.pdf"), bbox_inches = 'tight') 
-      else:
-        print("ERROR: edge list dataframe is empty. Nothing more to do.") 
+					edges.to_csv(f"{out_dir}/edge_list_{pref}.tsv", index = None, sep = '\t', mode = 'w')
+					network = data_net.plot_network()
+					network.savefig(os.path.join(out_dir, "network.pdf"), bbox_inches = 'tight')
+					
+					degree_histogram = data_net.plot_degree_hist()
+					degree_histogram.savefig(os.path.join(out_dir, "histogram.pdf"), bbox_inches = 'tight') 
+			else:
+				print("ERROR: edge list dataframe is empty. Nothing more to do.") 
 if __name__ == "__main__":
-  main()
+	main()
--- edge2net.py
+++ edge2net.py
@@ -8,111 +8,112 @@ import matplotlib.pyplot as plt
 import collections
 import numpy as np
 import os
-     
+		 
 class net():
-    def __init__(self, data):
-        self.data = data
-        # import edge list
-        # transform edge list (data frame) to edge list (networkx)
-        self.df = data
-        self.df["color"] = np.where(self.df['direction'] == "1", '#E3CD81FF', '#9E1030FF')
-        self.edgelist = nx.from_pandas_edgelist(self.df, source = 'source', target = 'target')
-    
-    def nodes_and_edges(self):
-        return(nx.number_of_nodes(self.edgelist), nx.number_of_edges(self.edgelist))
-    
-    def plot_network(self, pltTitle = "Default"):
-        # set figure size
-        f = plt.figure(figsize=(10, 10))
+		def __init__(self, data):
+				self.data = data
+				# import edge list
+				# transform edge list (data frame) to edge list (networkx)
+				self.df = data
+				self.df["color"] = np.where(self.df['direction'] == "1", '#2A9D8F', '#E76F51')
+				self.edgelist = nx.from_pandas_edgelist(self.df, source = 'source', target = 'target')				
+			
 
-        #set layout to circular layout
-        #layout = nx.circular_layout(self.edgelist)
-        layout = nx.spring_layout(self.edgelist, k=0.15,iterations=20)
-        
-        # Go through every zotu -> how many connections?
-        # calculate circle size according to number of connections
-        zotus = list(self.df.source.unique())
-        zotu_size = [self.edgelist.degree(zotu) * 10 for zotu in zotus]
+		def nodes_and_edges(self):
+				return(nx.number_of_nodes(self.edgelist), nx.number_of_edges(self.edgelist))
+		
+		def plot_network(self, pltTitle = "Default"):
+				# set figure size
+				f = plt.figure(figsize=(10, 10))
 
-        # draw all nodes based on previously determined properties
-        nx.draw_networkx_nodes(self.edgelist, 
-                               layout, 
-                               nodelist = zotus, 
-                               #node_size = zotu_size,
-                               node_size = 7, 
-                               node_color = '#89ABE3FF')
+				#set layout to circular layout
+				layout = nx.circular_layout(self.edgelist)
+				#layout = nx.spring_layout(self.edgelist, k=0.15,iterations=20)
+				
+				# Go through every zotu -> how many connections?
+				# calculate circle size according to number of connections
+				zotus = list(self.df.source.unique())
+				zotu_size = [self.edgelist.degree(zotu) * 5 for zotu in zotus]
+				
+				# draw all nodes based on previously determined properties
+				nx.draw_networkx_nodes(self.edgelist, 
+															 layout,
+															 nodelist = zotus, 
+															 node_size = zotu_size,
+															 #node_size = 7, 
+															 node_color = '#89ABE3FF')
 
-        # draw all edges based on previously determined properties
-        nx.draw_networkx_edges(self.edgelist, layout, width = 1, edge_color = self.df["color"])
-        
-        # set some plot specific elements
-        plt.axis('off')
-        plt.title(pltTitle)
+				# draw all edges based on previously determined properties
+				nx.draw_networkx_edges(self.edgelist, layout, width = 1, edge_color = self.df["color"])
+				
+				# set some plot specific elements
+				plt.axis('off')
+				plt.title(pltTitle)
 
-        # return the network plot
-        return(f)
-        
-    def get_density_value(self):
-        # https://en.wikipedia.org/wiki/Degree_distribution
-        # Density: The proportion of direct ties in a network relative to the total number possible.
-        return(f"Density: {round(nx.density(self.edgelist), 3)}")
-    
-    def get_degree_values(self, sort = True):
-        degrees = list(nx.degree(self.edgelist))
-        if sort : degrees.sort(key = lambda x:x[1])
-        return(degrees)
-    
-    def get_network_info(self):
-        return(nx.info(self.edgelist))
+				# return the network plot
+				return(f)
+				
+		def get_density_value(self):
+				# https://en.wikipedia.org/wiki/Degree_distribution
+				# Density: The proportion of direct ties in a network relative to the total number possible.
+				return(f"Density: {round(nx.density(self.edgelist), 3)}")
+		
+		def get_degree_values(self, sort = True):
+				degrees = list(nx.degree(self.edgelist))
+				if sort : degrees.sort(key = lambda x:x[1])
+				return(degrees)
+		
+		def get_network_info(self):
+				return(nx.info(self.edgelist))
 
-    def relation_info(self):
-        positive = len(self.df[self.df["direction"] == "1"])
-        negative = len(self.df[self.df["direction"] == "-1"])
+		def relation_info(self):
+				positive = len(self.df[self.df["direction"] == "1"])
+				negative = len(self.df[self.df["direction"] == "-1"])
 
-        # test if number of edges are equal before reporting informations about relations
-        if ((positive + negative) == nx.number_of_edges(self.edgelist)):
-          return(f"Positive: {positive}\t Negative: {negative}")
-        else:
-          return("ERROR: Number of edges differ.")
-    
-    def summary(self):
-        return([self.get_network_info(), self.relation_info(), self.get_density_value()])
-    
-    def plot_degree_hist(self):
-        # set subplot properties
-        fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(8,3))
-        fig.suptitle('Degree distribution and relation')
-        
-        degree_freq = nx.degree_histogram(self.edgelist)
-        degrees = range(len(degree_freq))
-        
-        m = 3
-        ax1.loglog(degrees[m:], degree_freq[m:], color = "#89ABE3FF")  
-        ax1.set_xlabel('Degree')
-        ax1.set_ylabel('Frequency')
-        ax1.spines["top"].set_visible(False)
-        ax1.spines["right"].set_visible(False)
+				# test if number of edges are equal before reporting informations about relations
+				if ((positive + negative) == nx.number_of_edges(self.edgelist)):
+					return(f"Positive: {positive}\t Negative: {negative}")
+				else:
+					return("ERROR: Number of edges differ.")
+		
+		def summary(self):
+				return([self.get_network_info(), self.relation_info(), self.get_density_value()])
+		
+		def plot_degree_hist(self):
+				# set subplot properties
+				fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(8,3))
+				fig.suptitle('Degree distribution and relation')
+				
+				degree_freq = nx.degree_histogram(self.edgelist)
+				degrees = range(len(degree_freq))
+				
+				m = 3
+				ax1.loglog(degrees[m:], degree_freq[m:], color = "#89ABE3FF")  
+				ax1.set_xlabel('Degree')
+				ax1.set_ylabel('Frequency')
+				ax1.spines["top"].set_visible(False)
+				ax1.spines["right"].set_visible(False)
 
-        # https://networkx.github.io/documentation/stable/auto_examples/drawing/plot_degree_histogram.html
-        degree_sequence = sorted([d for n, d in self.edgelist.degree()], reverse = True)  # degree sequence
-        degreeCount = collections.Counter(degree_sequence)
-        deg, cnt = zip(*degreeCount.items())
+				# https://networkx.github.io/documentation/stable/auto_examples/drawing/plot_degree_histogram.html
+				degree_sequence = sorted([d for n, d in self.edgelist.degree()], reverse = True)	# degree sequence
+				degreeCount = collections.Counter(degree_sequence)
+				deg, cnt = zip(*degreeCount.items())
 
-        ax2.bar(deg, cnt, width=0.80, color='#89ABE3FF')
-        ax2.set_ylabel('Count')
-        ax2.set_xlabel('Degree')
-        ax2.spines["top"].set_visible(False)
-        ax2.spines["right"].set_visible(False)   
-        
-        # get number of positive and negative relations (same as function -> relation.info())
-        positive = len(self.df[self.df["direction"] == "1"])
-        negative = len(self.df[self.df["direction"] == "-1"])
-        
-        ax3.bar(["Pos", "Neg"], [positive,negative], width=0.80, color='#89ABE3FF')
-        ax3.set_ylabel('Count')
-        ax3.set_xlabel('Relation')
-        ax3.spines["top"].set_visible(False)
-        ax3.spines["right"].set_visible(False)   
-        
-        return(fig)
+				ax2.bar(deg, cnt, width=0.80, color='#89ABE3FF')
+				ax2.set_ylabel('Count')
+				ax2.set_xlabel('Degree')
+				ax2.spines["top"].set_visible(False)
+				ax2.spines["right"].set_visible(False)	 
+				
+				# get number of positive and negative relations (same as function -> relation.info())
+				positive = len(self.df[self.df["direction"] == "1"])
+				negative = len(self.df[self.df["direction"] == "-1"])
+				
+				ax3.bar(["Pos", "Neg"], [positive,negative], width=0.80, color='#89ABE3FF')
+				ax3.set_ylabel('Count')
+				ax3.set_xlabel('Relation')
+				ax3.spines["top"].set_visible(False)
+				ax3.spines["right"].set_visible(False)	 
+				
+				return(fig)
 
